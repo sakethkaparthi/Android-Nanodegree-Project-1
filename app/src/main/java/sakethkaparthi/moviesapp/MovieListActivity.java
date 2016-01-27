c3830d0c227c732bf93141ff3285b5806239a1ed
sakethkaparthi <sakethkaparthi@outlook.com>
2016-01-27 19:03:04 +0530

    added options menu for different api calls

--- app/src/main/java/sakethkaparthi/moviesapp/MovieListActivity.java
+++ app/src/main/java/sakethkaparthi/moviesapp/MovieListActivity.java
@@ -4,6 +4,9 @@ import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.GridView;
 
 import java.util.ArrayList;
@@ -14,18 +17,63 @@ import retrofit.client.Response;
 
 public class MovieListActivity extends AppCompatActivity {
     MovieAdapter movieAdapter;
+    ProgressDialog progressDialog;
+    GridView gridview;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.sort_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.sort_by_popular) {
+            sortByPopular();
+            return true;
+        } else if (item.getItemId() == R.id.sort_by_ratings) {
+            sortByRatings();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_list);
-        final GridView gridview = (GridView) findViewById(R.id.grid_view);
-
-        final ProgressDialog progressDialog = new ProgressDialog(this);
+        gridview = (GridView) findViewById(R.id.grid_view);
+        progressDialog = new ProgressDialog(this);
         progressDialog.setMessage("Loading...");
-        progressDialog.setCancelable(false);
+        progressDialog.setCancelable(true);
         progressDialog.show();
+        sortByPopular();
+    }
+
+    void sortByPopular() {
         APIClient.getAPI().getPopularMovies(new Callback<MoviesModel>() {
+            @Override
+            public void success(MoviesModel moviesModel, Response response) {
+                progressDialog.dismiss();
+                ArrayList<Movie> movies = new ArrayList<Movie>();
+                movies.addAll(moviesModel.getResults());
+                movieAdapter = new MovieAdapter(getApplicationContext(), movies);
+                gridview.setAdapter(movieAdapter);
+                movieAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                progressDialog.dismiss();
+                error.printStackTrace();
+            }
+        });
+    }
+
+    void sortByRatings() {
+        APIClient.getAPI().getTopRatedMovies(new Callback<MoviesModel>() {
             @Override
             public void success(MoviesModel moviesModel, Response response) {
                 progressDialog.dismiss();
@@ -44,6 +92,5 @@ public class MovieListActivity extends AppCompatActivity {
                 error.printStackTrace();
             }
         });
-
     }
 }
