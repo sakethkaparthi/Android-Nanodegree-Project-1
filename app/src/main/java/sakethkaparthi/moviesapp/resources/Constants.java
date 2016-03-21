eb5b01c256817ee51e75a77397c68fefbd410fdd
sakethkaparthi <sakethkaparthi@outlook.com>
2016-03-21 23:08:11 +0530

    api key

--- app/src/main/java/sakethkaparthi/moviesapp/resources/Constants.java
+++ app/src/main/java/sakethkaparthi/moviesapp/resources/Constants.java
@@ -6,6 +6,6 @@ package sakethkaparthi.moviesapp.resources;
 public class Constants {
     public static final String BASE_URL="http://api.themoviedb.org/3" ;
     public static final String IMAGE_BASE_URL="http://image.tmdb.org/t/p/w185/";
-    public static final String API_KEY = "d4dc650ab1e0ee61a3a4e17453f770f5";
+    public static final String API_KEY = "YOUR KEY HERE";
     public static final String YOUTUBE_BASE = "https://www.youtube.com/watch?v=";
 }
